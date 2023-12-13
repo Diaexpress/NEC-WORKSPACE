2b8c76cacd6d8837aa0241b1f05e7afe9da337d3
Diaexpress <83020359+Diaexpress@users.noreply.github.com>
2023-12-13 09:58:57 +0000

    Update next.config.js
    first changes by me

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'NEC SYSTEMS',
+    description: 'Your official growth partner.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://Diaexpress.github.io/NEC-WORKSPACE/',
+    contactUrl: 'https://github.com/Diaexpress/NEC-WORKSPACE/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
